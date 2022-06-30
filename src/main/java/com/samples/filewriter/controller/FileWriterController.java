a80678913860d855f17cbaeb22b6a93f29612f88
Dinesh Edirisinghe <dineshed25@gmail.com>
2022-06-30 21:07:09 +0530

    first commit

---
+++ src/main/java/com/samples/filewriter/controller/FileWriterController.java
@@ -0,0 +1,8 @@
+package com.samples.filewriter.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FileWriterController {
+
+}
---
+++ src/main/java/com/samples/filewriter/service/FileWriterService.java
@@ -0,0 +1,8 @@
+package com.samples.filewriter.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FileWriterService {
+
+}
